9557cf9e53c57715a949975dc098f6f1d5868235
Martynas Petuška <martynas.petuska@outlook.com>
2021-02-06 14:35:33 +0200

    Alert module

---
+++ lib/src/main/kotlin/io/github/mpetuska/khakra/alert/Alert.kt
@@ -0,0 +1,42 @@
+@file:JsModule("@chakra-ui/alert/dist/esm/alert")
+
+package io.github.mpetuska.khakra.alert
+
+import io.github.mpetuska.khakra.system.ComponentWithAs
+import io.github.mpetuska.khakra.system.HTMLChakraProps
+import io.github.mpetuska.khakra.system.ThemingProps
+import react.RClass
+
+public external interface AlertOptions {
+  /**
+   * The status of the alert
+   *
+   * type: `"success" | "warning" | "error" | "info"`
+   */
+  public var status: String?
+    get() = definedExternally
+    set(value) = definedExternally
+}
+
+public external interface AlertProps
+  : HTMLChakraProps,
+  AlertOptions,
+  ThemingProps {}
+
+/**
+ * Alert is used to communicate the state or status of a
+ * page, feature or action
+ */
+public external val Alert: ComponentWithAs<AlertProps>
+
+public external interface AlertTitleProps : HTMLChakraProps
+
+public external val AlertTitle: ComponentWithAs<AlertTitleProps>
+
+public external interface AlertDescriptionProps : HTMLChakraProps
+
+public external val AlertDescription: ComponentWithAs<AlertDescriptionProps>
+
+public external interface AlertIconProps : HTMLChakraProps
+
+public external val AlertIcon: RClass<AlertDescriptionProps>
---
+++ lib/src/main/kotlin/io/github/mpetuska/khakra/alert/icons.kt
@@ -0,0 +1,12 @@
+@file:JsModule("@chakra-ui/alert/dist/esm/icons")
+
+package io.github.mpetuska.khakra.alert
+
+import io.github.mpetuska.khakra.icon.IconProps
+import react.RClass
+
+public external val CheckIcon: RClass<IconProps>
+
+public external val InfoIcon: RClass<IconProps>
+
+public external val WarningIcon: RClass<IconProps>
